0b8fcd5e92b540b9a14895a03d903730d52a5df9
julesbertrand <julesbertrand13@gmail.com>
2021-02-21 14:52:04 +0100

    config: config files simplified

--- config/config.py
+++ config/config.py
@@ -1,8 +1,10 @@
 # Data retrieval
 API_KEYS_FILEPATH = "./secrets/api_keys.yaml"
 API_ENDPOINTS = {
-    "FRED_API_URL_OBS": "https://api.stlouisfed.org/fred/series/observations?",
-    "FRED_API_URL_SER": "https://api.stlouisfed.org/fred/series?",
-    "USBLS_API_URL": "https://api.bls.gov/publicAPI/v2/timeseries/data/",
+    "FRED_OBS": "https://api.stlouisfed.org/fred/series/observations?",
+    "FRED_SER": "https://api.stlouisfed.org/fred/series?",
+    "FRED": "https://api.stlouisfed.org/fred/",
+    "USBLS": "https://api.bls.gov/publicAPI/v2/timeseries/data/",
+    "OECD": "http://stats.oecd.org/SDMX-JSON/data/",
 }
 API_REQUESTS_PARAMS_FILEPATH = "./config/config_api.yaml"
